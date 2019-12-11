bb788a2a5d3dec0cb1cde753a1db13d8fa16d3df
Reddy57 <abhilash.gaddam@antra.com>
2019-12-11 16:30:51 -0500

    added login functionality

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/models/user';
+import { Login } from 'src/app/shared/models/login';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  invalidLogin: boolean;
+  returnUrl: string;
+  user: User;
+  userLogin: Login = {
+    password: '',
+    username: ''
+  };
+  constructor(private authService: AuthenticationService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams
+      .subscribe(params => this.returnUrl = params['returnUrl'] || '/');
+  }
+
+  login() {
+    this.authService.login(this.userLogin)
+      .subscribe((response) => {
+        if (response) {
+          // console.log(' this is returnURL: ' + this.returnUrl);
+          this.router.navigate([this.returnUrl]);
+        } else {
+          this.invalidLogin = true;
+        }
+      },
+        (err: any) => { this.invalidLogin = true, console.log(err); });
+
   }
 
 }
--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  favoritesCount: number;
+  myMoviesCount: number;
+  currentUser: User;
+  isAuthenticated: boolean;
+
+  constructor(public authService: AuthenticationService, private router: Router, ) { }
 
   ngOnInit() {
+  }
+  logout() {
+
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
   }
 
 }
