24a08d21f3807811ba873fe3fc9c7ff3cbe245be
Reddy57 <abhilash.gaddam@antra.com>
2020-04-23 20:52:26 -0400

    added signalR realtime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from './core/services/authentication.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "./core/services/authentication.service";
+import { MovieShopSignalRService } from "./core/realtime/movie-shop-signal-r.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  title = 'MoviePlay';
-
-  constructor(private authService: AuthenticationService) {
-  }
+  title = "MoviePlay";
+  discountMessage: string;
+  constructor(
+    private authService: AuthenticationService,
+    private signalRService: MovieShopSignalRService
+  ) {}
 
   ngOnInit() {
     this.authService.populateUserInfo();
+    this.signalRService.startConnection();
+    this.signalRService.messageReceived$.subscribe((data) => {
+      this.discountMessage = data;
+      console.log(this.discountMessage)
+
+    });
   }
 }
